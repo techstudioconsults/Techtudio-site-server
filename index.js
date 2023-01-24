1839872e7110b44432cf77c165c22c9a870d1001
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-24 13:04:54 +0100

    feat: added a whitelist of servers that will be using the APIs

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+//import utils
+const corsOptions = require("./utils/corsOptions");
+
 //import custom middlewares
 
 //import custom routes
 
 //Define middlewares
 const app = express();
-const PORT = process.env.PORT || 5077;
+const PORT = process.env.PORT || 5000;
 
 //use middlewares
-app.use(
-    cors({
-      origin: "*"
-    })
-  );
-  app.use(express.json());
-  app.use(bodyParser.json());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  
-  app.get("/api", (req, res) => {
-    res.send("Welcome to Techstudio server");
-  });
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api", (req, res) => {
+  res.send("Welcome to Techstudio server");
+});
 
 //use custom middlewares
 
-
 //connect to server
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
---
+++ utils/whiteList.js
@@ -0,0 +1,7 @@
+//add servers consuming API here
+const whiteList = [
+    'http://localhost:3500',
+    'http://localhost:3000'
+];
+
+module.exports = whiteList;
