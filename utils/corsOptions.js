400f1192a72240ed3a7b848f53dabc3b70c5f8a9
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-31 10:37:36 +0100

    fix: CORS error fix

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -4,7 +4,7 @@ const inProduction = process.env.NOD_ENV === "production";
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1 || (!inProduction && !origin)) {
+    if (whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
