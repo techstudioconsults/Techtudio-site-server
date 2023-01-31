4c0d8360edce3be13265dabe90b832c65edf65e3
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-31 22:36:44 +0100

    fix: CORS options IP address error finally fixed

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,18 +1,5 @@
 const { whiteList, whiteListIp } = require("./whiteList");
 
-const inProduction = process.env.NOD_ENV === "production";
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whiteList.indexOf(origin) !== -1 || (!inProduction && !origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   optionsSuccessStatus: 200,
-// };
-
 const corsOptionsDelegate = function (req, callback) {
   const corsOptions = {
     methods: ["GET", "PUT", "POST", "DELETE", "HEAD", "PATCH"],
