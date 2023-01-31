6ce3a7476b569190481f402ee77037a8f55941f0
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-31 10:27:26 +0100

    feat: I added the facebook Ip to the whitelist

--- utils/whiteList.js
+++ utils/whiteList.js
@@ -3,7 +3,8 @@ const whiteList = [
     'http://localhost:3500',
     'http://localhost:3000',
     'https://techstudio-site.vercel.app/',
-    'https://techstudio-site-techstudioconsults.vercel.app/'
+    'https://techstudio-site-techstudioconsults.vercel.app/',
+    process.env.BASE_URL
 ];
 
 module.exports = whiteList;
