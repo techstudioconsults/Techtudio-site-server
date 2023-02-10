1da992db2257f39485aac3aafb461673f624e4f7
Tobi Olanitori <tobiolanitori@gmail.com>
2023-02-10 15:08:07 +0100

    feat: allowed cors for all urls

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //import dependencies here
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const compression = require('compression');
-const mongoose = require('mongoose');
+const compression = require("compression");
+const mongoose = require("mongoose");
 
 //import utils
 const corsOptionsDelegate = require("./utils/corsOptions");
-const connectDB = require('./utils/dbConnect'); 
+const connectDB = require("./utils/dbConnect");
 
 //import custom middlewares
 
 //import custom routes
-const facebookAd = require('./routes/facebookAdRouter')
-const auth = require('./routes/authRouter');
+const facebookAd = require("./routes/facebookAdRouter");
+const auth = require("./routes/authRouter");
 
 //Define middlewares
 const app = express();
@@ -23,7 +23,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 //use middlewares
-app.use(cors(corsOptionsDelegate));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
@@ -36,39 +40,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
 //static assests
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 //use custom middlewares
-
 
 //use routes
 app.get("/api", (req, res) => {
   res.send("Welcome to Techstudio server");
 });
 
-app.use('/api/facebookAd', facebookAd)
-app.use('/api/auth', auth);
-
-
+app.use("/api/facebookAd", facebookAd);
+app.use("/api/auth", auth);
 
 //invalid routes
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
   res.status(404);
-  if (req.accepts('html')) {
+  if (req.accepts("html")) {
     // console.log('html')
-      res.sendFile(path.join(__dirname, 'public', '404.html'));
-  } else if (req.accepts('json')) {
-    console.log('json')
-      res.json({ "error": "404 Not Found" });
+    res.sendFile(path.join(__dirname, "public", "404.html"));
+  } else if (req.accepts("json")) {
+    console.log("json");
+    res.json({ error: "404 Not Found" });
   } else {
-      res.type('txt').send("404 Not Found");
+    res.type("txt").send("404 Not Found");
   }
 });
 
-
-
 //connect to server
-mongoose.connection.once('open', ()=> {
-  console.log('Connected to DB')
-  app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
-})
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
