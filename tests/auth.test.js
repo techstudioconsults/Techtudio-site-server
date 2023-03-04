1d2a8f8a826dcd174a11f8f8c86b4d1328d319cf
Tobi Olanitori <tobiolanitori@gmail.com>
2023-03-04 20:50:25 +0100

    test: test for change of password done

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -65,6 +65,7 @@ describe("Test authentication endpoints", () => {
   let adminToken;
   let studentToken;
   let tutorToken;
+  let refreshToken;
 
   describe("POST /api/auth/register/admin", () => {
     test("It should respond with 201 when an admin is created", async () => {
@@ -126,6 +127,7 @@ describe("Test authentication endpoints", () => {
         .post("/api/auth/login")
         .send({ email: adminInfo.email, password: adminInfo.password });
       adminToken = res._body.data.accessToken;
+      refreshToken = res._body.data.refreshToken;
       expect(res.status).toBe(200);
       expect(res._body.success).toBe(true);
       expect(res._body.data.accessToken).toBeDefined();
@@ -230,15 +232,15 @@ describe("Test authentication endpoints", () => {
         .post("/api/auth/signup")
         .set("Authorization", `Bearer ${adminToken}`)
         .send({
-            firstName: "student",
-            lastName: "studentLast",
-            email: "student@email.com",
-            password: "password",
-            phoneNumber: "1234",
-            schedule: "weekday",
-            course: "backend",
-            userRole: "STUDENT",
-          });
+          firstName: "student",
+          lastName: "studentLast",
+          email: "student@email.com",
+          password: "password",
+          phoneNumber: "1234",
+          schedule: "weekday",
+          course: "backend",
+          userRole: "STUDENT",
+        });
       expect(res.status).toBe(201);
       expect(res._body.success).toBe(true);
     });
@@ -266,19 +268,84 @@ describe("Test authentication endpoints", () => {
     });
 
     test("It should return a 403 when role gotten from token is not that of an Admin", async () => {
-        const res = await request(app)
+      const res = await request(app)
         .post("/api/auth/signup")
         .set("Authorization", `Bearer ${tutorToken}`)
         .send({
-            firstName: "tutor",
-            lastName: "tutorLast",
-            email: "tutor1@email.com",
-            password: "password",
-            phoneNumber: 1234,
-            userRole: "TUTOR",
-          });
+          firstName: "tutor",
+          lastName: "tutorLast",
+          email: "tutor1@email.com",
+          password: "password",
+          phoneNumber: 1234,
+          userRole: "TUTOR",
+        });
       expect(res.status).toBe(403);
       expect(res._body.success).toBe(false);
+    });
+  });
+
+  describe("POST /api/auth/token", () => {
+    test("it should return a 400 if no refresh token in req body", async () => {
+      const res = await request(app).post("/api/auth/token").send();
+      expect(res.status).toBe(400);
+    });
+
+    test("it should return a 403 when refresh token is bad", async () => {
+      const res = await request(app)
+        .post("/api/auth/token")
+        .send({ refreshToken: "uuwy727278332211" });
+      expect(res.status).toBe(403);
+    });
+
+    test("It should return a 201 and a new access token", async () => {
+      const res = await request(app)
+        .post("/api/auth/token")
+        .send({ refreshToken });
+      console.log(res);
+      expect(res.status).toBe(201);
+      expect(res._body.accessToken).toBeDefined();
+    });
+
+    test("it should return a 403 if refresh token is out of rotation", async () => {
+      //find refresh token and simulate it out of rotation
+      const user = await Profile.findOne({ email: adminInfo.email }).select(
+        "refreshToken role"
+      );
+      user.refreshToken = [];
+      await user.save();
+
+      //make the test
+      const res = await request(app)
+        .post("/api/auth/token")
+        .send({ refreshToken });
+      expect(res.status).toBe(403);
+    });
+  });
+
+  describe("POST /api/auth/change-password", () => {
+    test("It should return a 401 when request is sent without token", async () => {
+      const res = await request(app)
+        .patch("/api/auth/change-password")
+        .send({ oldPassword: adminInfo.password, newPassword: "newPasssword" });
+      expect(res.status).toBe(401);
+    });
+
+    test("It should return a 401 when old password does not match DB records", async () => {
+      const res = await request(app)
+        .patch("/api/auth/change-password")
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send({ oldPassword: "new", newPassword: "newPasssword" });
+      expect(res.status).toBe(401);
+      expect(res._body.success).toBe(false);
+    });
+
+    test("It should return a 201 when the password change is successful", async () => {
+      const res = await request(app)
+        .patch("/api/auth/change-password")
+        .set("Authorization", `Bearer ${adminToken}`)
+        .send({ oldPassword: adminInfo.password, newPassword: "newPasssword" });
+      expect(res.status).toBe(201);
+      expect(res._body.success).toBe(true);
     });
   });
 });
